2dee5e1f22ca6274cfd3d284a4f64e27b0f8eb28
Anabel Chacón <achacofe@mercadona.es>
2023-10-19 23:35:44 +0200

    feat: remove unused method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  isOpenSidebar!: boolean;
-  isOpenMenu(event: boolean) {
-    this.isOpenSidebar = event;
-  }
-}
+export class AppComponent {}
